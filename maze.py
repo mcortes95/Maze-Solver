b01cf413c635ed6568989a23a3776fec84565c5d
Marco Cortes <16551788+mcortes95@users.noreply.github.com>
2018-11-13 19:49:34 -0600

    more

--- maze.py
+++ maze.py
@@ -3,9 +3,14 @@ from PIL import Image
 def shortpathmaze(maze):
     largerimage=maze.resize((1500,1500))
     largerimage.show()
+    height=maze.height
+    width=maze.width
+    print(height,width)
+    for x in height:
+        print(x)
 
-
-testmaze=Image.open('maze0.png')
+maze_name=input("Enter the name of the maze: ")
+testmaze=Image.open(maze_name)
 rgb=(testmaze.getpixel((1,0)))
 print(rgb)
 shortpathmaze(testmaze)
